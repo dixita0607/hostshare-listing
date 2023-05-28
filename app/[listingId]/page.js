a5ccc76bc02ddeb0c17dd4bee235140b31bee4a6
Dixita Ganatra <contact@dixita.dev>
2023-05-28 19:25:56 +0530

    Add content to details page

--- app/[listingId]/page.js
+++ app/[listingId]/page.js
@@ -1,7 +1,82 @@
+import Image from "next/image";
+import Header from "../components/Header";
+import allListings from "../data/listing-data";
+
 export default function ListingDetails({ params }) {
+  const listingDetails = allListings.data.find(
+    (house) => house.info.id === params.listingId
+  )?.info;
+
   return (
-    <div className="flex items-center p-24">
-      Listing Details of {params.listingId}
+    <div className="flex flex-col">
+      <Header />
+      {/* Title */}
+      <div className="flex flex-col">
+        <h1>{listingDetails.title}</h1>
+        <div className="flex align-middle">
+          <div>{listingDetails.ratings.guestSatisfactionOverall}</div>
+          <div>{listingDetails.visibleReviewCount}</div>
+          {listingDetails.host.isSuperhost && <div>Superhost</div>}
+          <div>
+            {listingDetails.location.city},{" "}
+            {listingDetails.location.country.title}
+          </div>
+        </div>
+      </div>
+      {/* Images */}
+      <div className="flex align-middle justify-start flex-wrap">
+        {listingDetails.images.data?.map((image) => (
+          <div key={image.url} className="p-2 m-2">
+            <Image
+              // TODO: Temporary use 100x100
+              // width={image.width}
+              // height={image.height}
+              width={100}
+              height={100}
+              alt="Interior"
+              src={image.url}
+            />
+          </div>
+        ))}
+      </div>
+      {/* Host info */}
+      <div className="flex align-middle justify-between">
+        <h1>
+          {listingDetails.type} hosted by {listingDetails.host.name}
+        </h1>
+        <div>
+          <Image
+            src={listingDetails.host.avatar.url}
+            width={listingDetails.host.avatar.width}
+            height={listingDetails.host.avatar.height}
+            alt="Host"
+          />
+        </div>
+      </div>
+      {/* TODO: Render this in markdown or html */}
+      <div>{listingDetails.description}</div>
+
+      {/* Amenities */}
+      <div>
+        <h1>What this place offers</h1>
+        {listingDetails.amenities.data?.map((amenity) => (
+          <div key={amenity.name}>
+            {amenity.title} - {amenity.available ? "Yes" : "No"}
+          </div>
+        ))}
+      </div>
+
+      {/* Location */}
+      <div>
+        {listingDetails.location.city}, {listingDetails.location.country.title}
+      </div>
+
+      {/* Reserve */}
+      {/* TODO: Design whole card */}
+      <div className="flex align-middle justify-between fixed right-0">
+        {listingDetails.currency.symbol}
+        {listingDetails.price}/night
+      </div>
     </div>
   );
 }
--- app/components/ListingCard.js
+++ app/components/ListingCard.js
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const ListingCard = ({ details }) => {
   return (
     <div className="p-2 m-2 border-solid rounded border-black	border-2">
-      <Image
-        src={details.mainImage.url}
-        width={200}
-        height={200}
-        alt="Hostshare Logo with name"
-      />
-      <div className="flex align-middle justify-between">
-        <div>{details.location.city}</div>
-        <div>
-          {details.currency.symbol}
-          {details.price}/night
+      <Link href={`/${details.id}`}>
+        <Image
+          src={details.mainImage.url}
+          width={200}
+          height={200}
+          alt="Hostshare Logo with name"
+        />
+        <div className="flex align-middle justify-between">
+          <div>{details.location.city}</div>
+          <div>
+            {details.currency.symbol}
+            {details.price}/night
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
