d0b930bb9b4848cccc7fb20a9cdaaa2eb6b9b5ae
Dixita Ganatra <contact@dixita.dev>
2023-05-29 22:06:16 +0530

    Add react-icons
     - Use profile and menu icons in the header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import Search from "./Search";
 import Link from "next/link";
+import { CgProfile, CgMenu } from "react-icons/cg";
 
 const Header = () => {
   return (
-    <div className="flex align-middle justify-between p-8">
+    <div className="flex align-middle justify-between p-5 pl-32 pr-32 border-b-2 border-gray-200 border-solid">
       <Link href="/">
         <Image
           src="/images/logo-with-name.png"
-          width={200}
-          height={200}
+          width={180}
+          height={180}
           alt="Hostshare Logo with name"
         />
       </Link>
       <Search />
-      <div>Profile and Menu</div>
+      <div className="flex align-middle justify-between">
+        <CgMenu className="text-2xl" />
+        <CgProfile className="ml-4 text-2xl" />
+      </div>
     </div>
   );
 };
