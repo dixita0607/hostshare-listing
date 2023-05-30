23e7af3596de920e1ac8742999b2496c38c768be
Dixita Ganatra <contact@dixita.dev>
2023-05-30 21:47:58 +0530

    Change font-family

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
 import Header from "./components/Header";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
   title: "Hostshare by Dixita",
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Header />
         {children}
       </body>
